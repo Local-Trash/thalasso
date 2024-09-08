0a08855020c265707265bfb53cb28720588454b8
LocalTrash <tqdagboy@gmail.com>
2024-09-07 19:20:50 -0700

    moving to llvm-sys

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-
-    println!("cargo::rustc-link-search=native={}", dir);
-    println!("cargo::rustc-link-lib=static=LLVM-C");
-}
